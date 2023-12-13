5261cd4d51ed7585ac752f309f80f3ed2fd38fff
Ernie <ernienishino@gmail.com>
2023-12-13 18:41:59 +0800

    基本数据处理

--- anime_program/pages/index/index.js
+++ anime_program/pages/index/index.js
@@ -1,52 +1,20 @@
 // index.js
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+const animeData = require('../../data/animeData.json'); // 请替换为实际的数据路径
 
-Component({
+Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
-    },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    showAnime: [],
   },
-  methods: {
-    // 事件处理函数
-    bindViewTap() {
-      wx.navigateTo({
-        url: '../logs/logs'
-      })
-    },
-    onChooseAvatar(e) {
-      const { avatarUrl } = e.detail
-      const { nickName } = this.data.userInfo
-      this.setData({
-        "userInfo.avatarUrl": avatarUrl,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
-    },
-    onInputChange(e) {
-      const nickName = e.detail.value
-      const { avatarUrl } = this.data.userInfo
-      this.setData({
-        "userInfo.nickName": nickName,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
-    },
-    getUserProfile(e) {
-      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-      wx.getUserProfile({
-        desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-        success: (res) => {
-          console.log(res)
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    },
+  showAnimeByDay(e) {
+    const day = e.currentTarget.dataset.day;
+    const filteredAnime = animeData.filter(item => item.updateDay === day);
+    this.setData({ showAnime: filteredAnime });
   },
-})
+  goToDetailPage(e) {
+    const index = e.currentTarget.dataset.index;
+    const selectedAnime = this.data.showAnime[index];
+    wx.navigateTo({
+      url: `/pages/detail/detail?name=${selectedAnime.name}&cover=${selectedAnime.cover}&type=${selectedAnime.type}&actors=${selectedAnime.actors}&production=${selectedAnime.production}`,
+    });
+  },
+});
