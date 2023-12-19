adaae3d5e3eb0d3695c38d8ace49e22f097523a2
Ernie <ernienishino@gmail.com>
2023-12-19 11:34:55 +0800

    第一版变更1

--- anime_program/pages/detail/detail.js
+++ anime_program/pages/detail/detail.js
@@ -1,26 +1,12 @@
 // detail.js
 Page({
   data: {
-    Name: "暂无",
-    Day: "暂无",
-    Type: "暂无",
-    Intro: "暂无",
-    Actors: "暂无",
-    Prod: "暂无",
-    Rate: "暂无"
+    animeDetail: {},
   },
   onLoad(options) {
-    this.setData({
-      Name: options.name,
-      Day: options.day,
-      Type: options.type,
-      Intro: options.intro,
-      Actors: options.actors,
-      Prod: options.prod,
-      Rate: options.rate
-    });
+    const { name, cover, type, actors, production } = options;
+    this.setData({ animeDetail: { name, cover, type, actors, production } });
   },
-  
   navigateBack: function () {
     wx.navigateBack();
   }
